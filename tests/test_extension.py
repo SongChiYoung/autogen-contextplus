36b751fb46d847e3fd5e0e85ad04de29039856b5
SongChiYoung <stchuck2008@gmail.com>
2025-04-24 09:50:50 +0900

    TEST: adding test with agent

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -1,8 +1,10 @@
 import pytest
-# import pytest_asyncio
-
 from typing import List
+
+from autogen_core import CancellationToken
 from autogen_core.models import UserMessage, LLMMessage
+from autogen_agentchat.base import TaskResult
+from autogen_agentchat.agents import AssistantAgent
 from autogen_ext.models.replay import ReplayChatCompletionClient
 from autogen_contextplus import ContextPlusChatCompletionContext
 from autogen_contextplus.extension.context import (
@@ -147,3 +149,64 @@ async def test_buffered_summary_chat_completion_context_serialize() -> None:
     assert deserialized_context._messages[0].content == "What is the capital of France?"  # type: ignore
     assert deserialized_context._messages[1].content == "SUMMARY1"  # type: ignore
     assert deserialized_context._messages[2].content == "Seoul"  # type: ignore
+
+
+@pytest.mark.asyncio
+async def test_buffered_cutoff_chat_completion_with_agent() -> None:
+    """Test the buffered cutoff chat completion context builder with agent."""
+    buffer_count = 1
+    max_messages = 1
+    messages = [
+        "What is the capital of France?",
+        "What is the capital of Korea?",
+    ]
+    responses = [
+        "Paris",
+        "Seoul",
+        "Paris",
+        "Seoul",
+    ]
+
+    async def _buffered_cutoff(agnet: AssistantAgent) -> None:
+        agent_responses: List[TaskResult] = []
+        for message in messages:
+            agent_responses.append(await agent.run(task=message))
+
+        print(f"[RESULTS] agent_responses: {agent_responses}")
+        print(f"[RESULTS] len(agent_responses): {len(agent_responses)}")
+
+        assert len(agent_responses) == 2
+        assert agent_responses[0].messages[1].to_text() == "Paris"
+        assert agent_responses[1].messages[1].to_text() == "Seoul"
+
+        assert len(context._messages) == 1  # type: ignore
+        assert len(context._non_modified_messages) == 4  # type: ignore
+        assert context._messages[0].content == "Seoul"  # type: ignore
+
+
+    client = ReplayChatCompletionClient(
+        chat_completions=responses
+    )
+
+    context = buffered_cutoff_chat_completion_context_builder(
+        buffer_count=buffer_count,
+        max_messages=max_messages,
+    )
+
+    agent = AssistantAgent(
+        "tester",
+        model_client=client,
+        system_message="You are a helpful agent",
+        model_context=context,
+    )
+
+    await _buffered_cutoff(agent)
+
+    await agent.on_reset(cancellation_token=CancellationToken())
+    serialized_agent = agent.dump_component()
+    assert serialized_agent is not None
+
+    deserialized_agent = AssistantAgent.load_component(serialized_agent)
+    assert deserialized_agent is not None
+
+    await _buffered_cutoff(deserialized_agent)
